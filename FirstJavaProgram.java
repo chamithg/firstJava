3ad7188a15f640e9ca7b5acadc674876550c5f75
gamagegit <cgamage92@yahoo.com>
2022-08-03 02:08:39 +0530

    commit all

---
+++ FirstJavaProgram.java
@@ -0,0 +1,8 @@
+public class FirstJavaProgram{
+    public static void main (String[]args){
+        System.out.println("My name is chamith");
+        System.out.println("Im 30 years old");
+        System.out.println("My home town is colombo");
+
+    }
+}
